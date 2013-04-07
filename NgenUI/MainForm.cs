34ba28a120824dd87b7a9c39ea60395a671fa596
Slava Guzenko <slavagu.dev@gmail.com>
2013-04-07 23:39:12 +1000

    Initial commit of NgenUI

---
+++ NgenUI/MainForm.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Windows.Forms;
+using SlavaGu.NgenWrapper;
+
+namespace SlavaGu.NgenUI
+{
+    public partial class MainForm : Form
+    {
+        private NgenLauncher _ngenLauncher;
+
+        public MainForm()
+        {
+            LoadSettings();
+            InitializeComponent();
+        }
+
+        public bool AllowCancellation { get; private set; }
+        public string CancellationConfirmation { get; private set; }
+        public string Label1 { get; private set; }
+        public string Label2 { get; private set; }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                var myCp = base.CreateParams;
+                if (!AllowCancellation)
+                {
+                    // disable close button
+                    myCp.ClassStyle |= 0x200;
+                }
+                return myCp;
+            }
+        }
+
+        private void LoadSettings()
+        {
+            Label1 = GetAppSetting("Label1", "Processing...");
+            Label2 = GetAppSetting("Label2", "@assemblyName");
+            AllowCancellation = GetAppSetting("AllowCancellation", true);
+            CancellationConfirmation = GetAppSetting("CancellationConfirmation", "Do you want to cancel the process?");
+        }
+
+        private void MainFormLoad(object sender, EventArgs e)
+        {
+            Text = GetAppSetting("WindowTitle", "Ngen UI");
+            label1.Text = Label1;
+            label2.Text = "";
+            
+            if (!AllowCancellation)
+            {
+                cancelButton.Enabled = false;
+            }
+
+            progressBar.Minimum = 0;
+            progressBar.Step = 1;
+
+            var ngenAction = Program.Args[0].ToLowerInvariant();
+            var assembly = Program.Args[1];
+
+            RunNgen(ngenAction, assembly);
+        }
+
+        private void RunNgen(string ngenAction, string assembly)
+        {
+            _ngenLauncher = new NgenLauncher();
+            _ngenLauncher.Progress += OnNgenProgress;
+            _ngenLauncher.Exit += OnNgenExit;
+
+            switch (ngenAction)
+            {
+                case "install":
+                    _ngenLauncher.InstallAssembly(assembly);
+                    break;
+
+                case "uninstall":
+                    _ngenLauncher.UninstallAssembly(assembly);
+                    break;
+
+                case "display":
+                    _ngenLauncher.DisplayAssembly(assembly);
+                    break;
+
+                default:
+                    throw new ArgumentException("Ngen action not supported: " + ngenAction);
+            }
+        }
+
+        private void OnNgenProgress(object sender, NgenProgressEventArgs e)
+        {
+            BeginInvoke((MethodInvoker)(() => UpdateProgress(e)));
+        }
+
+        private void OnNgenExit(object sender, NgenExitEventArgs e)
+        {
+            Program.ExitCode = e.ExitCode.GetValueOrDefault();
+            _ngenLauncher.Dispose();
+            _ngenLauncher = null;
+            BeginInvoke((MethodInvoker)(Close));
+        }
+
+        private void UpdateProgress(NgenProgressEventArgs e)
+        {
+            label2.Text = Label2.Replace("@assemblyName", e.Assembly);
+
+            Debug.WriteLine("UpdateProgress ---> {0}/{1} {2} ", e.Current, e.Total, e.Assembly);
+
+            progressBar.Maximum = e.Total;
+            progressBar.PerformStep();
+        }
+
+        private void MainFormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_ngenLauncher == null)
+                return;
+
+            var answer = MessageBox.Show(CancellationConfirmation, Text, MessageBoxButtons.YesNo,
+                                         MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+
+            if (answer == DialogResult.Yes && _ngenLauncher != null)
+            {
+                _ngenLauncher.Cancel();
+            }
+            e.Cancel = true;
+        }
+
+        private void CancelClicked(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private static T GetAppSetting<T>(string key, T defaultValue)
+        {
+            var value = ConfigurationManager.AppSettings[key];
+            if (value != null)
+            {
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+            return defaultValue;
+        }
+
+    }
+}
---
+++ NgenUI/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace SlavaGu.NgenUI
+{
+    static class Program
+    {
+        public enum ExitCodes
+        {
+            Success = 0,
+            InvalidArguments = 90001,
+            FatalError = 90002,
+        }
+
+        public static string[] Args;
+        public static int ExitCode = (int)ExitCodes.Success;
+
+        // defines for commandline output
+        [DllImport("kernel32.dll")]
+        static extern bool AttachConsole(int dwProcessId);
+        private const int AttachParentProcess = -1;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static int Main(string[] args)
+        {
+            Args = args;
+
+            if (args.Length < 2)
+            {
+                // redirect console output to parent process;
+                // must be before any calls to Console.WriteLine()
+                AttachConsole(AttachParentProcess);
+
+                Console.WriteLine("");
+                Console.WriteLine("NgenUI is a user interface for ngen.exe (CLR Native Image Generator)");
+                Console.WriteLine("Usage: ngenui <ngen action> <assembly>, e.g. ngenui install MyAssembly.dll");
+                Console.WriteLine("Supported ngen actions: install, uninstall, display");
+                Console.WriteLine("To customize the interface edit NgenUI.exe.config");
+
+                // sending the enter key is not really needed, but otherwise the user thinks the app is still running by looking at the commandline. 
+                // The enter key takes care of displaying the prompt again.
+                SendKeys.SendWait("{ENTER}");
+                Environment.ExitCode = (int)ExitCodes.InvalidArguments;
+                Application.Exit();
+            }
+            else
+            {
+                try
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new MainForm());
+                }
+                catch (Exception ex)
+                {
+                    Trace.TraceError("Error: " + ex);
+                    ExitCode = (int)ExitCodes.FatalError;
+                }
+            }
+
+            Trace.TraceInformation("Done. ExitCode={0}", ExitCode);
+            Environment.ExitCode = ExitCode;
+            return ExitCode;
+        }
+    }
+}
